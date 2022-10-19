8893e0af5074903619c9f5af26b996c58b0bb106
Jenni Juurikka <c0juje01@students.oamk.fi>
2022-10-19 22:37:58 +0300

    Problem with 'save' function

--- server/index.js
+++ server/index.js
@@ -23,4 +23,24 @@ app.get("/", async function (req, res) {
     }
 })
 
+app.post("/new", async function(req, res) {
+    try {
+        const connection = await mysql.createConnection(config.db)
+        const [result,] = await connection.execute("INSERT INTO task (descript) VALUES (?)", [req.body.descript])
+        res.status(200).json({id: result.insertId})
+    } catch (err) {
+        res.status(500).json({error: err.message})
+    }
+})
+
+app.delete("/delete/:id", async function(req, res) {
+    try {
+        const connection = await mysql.createConnection(config.db)
+        await connection.execute("DELETE FROM task WHERE id = ?", [req.params.id])
+        res.status(200).json({id: req.params.id})
+    } catch (err) {
+        res.status(500).json({error: err.message})
+    }
+})
+
 app.listen(port)
--- src/App.js
+++ src/App.js
@@ -6,18 +6,45 @@ const URL = "http://localhost:3001";
 
 export default function App() {
   const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState("");
 
   useEffect(() => {
     axios.get(URL)
       .then((res) => {
-        setTasks(res.data);
+        setTasks(res.data)
       }).catch(error => {
-        alert(error.res.data.error);
+        alert(error.res.data.error)
       })
   }, [])
 
+  function save() {
+    const json = JSON.stringify({descript: task})
+    axios.post(URL + "new",json,{
+      headers: {
+        "Content-Type" : "application/json"
+      }
+    })
+    .then((res) => {
+      // Convert stringifyed JSON object back to JavaScript object
+      const addedobject = JSON.parse(json)
+      // Add id returned by server to object
+      addedobject.id = res.data.id
+      // Update state variable with newly added data
+      setTasks(tasks => [...tasks, addedobject])
+      setTask("")
+    }).catch(error => {
+      alert(error.res.data.error)
+    })
+  }
+
   return (
     <div>
+      <form>
+        <label>New task</label>
+        <input value={task} onChange={e => setTask(e.target.value)}/>
+
+        <button type='button' onClick={save}>Save</button>
+      </form>
       <h4>To-do list:</h4>
       <ol>
         {tasks.map(task => (
